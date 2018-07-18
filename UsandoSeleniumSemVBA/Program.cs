a73e380a066b97563d14ebc28398eeaf81be0994
Tomás <tomamais@gmail.com>
2018-07-17 21:25:16 -0400

    Selenium sem VBA. Aproveita ai!

--- UsandoSeleniumSemVBA/Program.cs
+++ UsandoSeleniumSemVBA/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,20 @@ namespace UsandoSeleniumSemVBA
     {
         static void Main(string[] args)
         {
+            var options = new InternetExplorerOptions
+            {
+                IgnoreZoomLevel = true
+            };
+            IWebDriver driver = new OpenQA.Selenium.IE.InternetExplorerDriver(options);
+
+            driver.Navigate().GoToUrl("https://www.tomasvasquez.com.br/blog");
+
+            IWebElement elemento = driver.FindElement(By.TagName("h1"));
+
+            Console.WriteLine("O título do site é: {0}", elemento.Text);
+            Console.Read();
+
+            driver.Quit();
         }
     }
 }
